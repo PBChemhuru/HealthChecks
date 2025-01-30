82c9493a4387ec986671930416b21dd7b1f0746a
PBChemhuru <chemhuru.panashe@gmail.com>
2025-01-31 01:13:53 +0200

    [ffix]ui is a bit better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LoginService } from './services/login.service';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginComponent],
+  standalone:true,
+  imports: [RouterOutlet, LoginComponent, NavbarComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(private loginService:LoginService){}
   title = 'HealthChecks';
+  isAuthenticated(): boolean {
+    return this.loginService.isAuthenticated();
+  }
 }
